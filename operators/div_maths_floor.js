c4d1287628cdf8df5ee1e7ae8e7fb93d02a915f3
Utkarsh Kushwaha <utkarsh.cyber01@gmail.com>
2026-08-25 10:34:15 +0530

    Add Math.floor division program

---
+++ operators/div_maths_floor.js
@@ -0,0 +1,12 @@
+let a = 17
+let b= 5
+
+var div= a/b
+
+console.log(`Without Using Math.floor :- ${div}`)
+
+div = Math.floor(div)
+
+console.log(`After Using Math.floor :- ${div}`)
+
+console.log(Math.floor(5.9) )
