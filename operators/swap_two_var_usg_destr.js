9dadd6e407885843a05db950c089b7c5f5385626
Utkarsh Kushwaha <utkarsh.cyber01@gmail.com>
2026-08-25 10:34:14 +0530

    Add variable swap using destructuring

---
+++ operators/swap_two_var_usg_destr.js
@@ -0,0 +1,16 @@
+let a = 5;
+let b = 10;
+
+
+
+console.log("Before Swap :--");
+console.log("a = " + a);
+console.log("b = " + b);
+
+
+[a,b]=[b,a]
+
+
+console.log("After Swap :--");
+console.log("a = " + a);
+console.log("b = " + b);
