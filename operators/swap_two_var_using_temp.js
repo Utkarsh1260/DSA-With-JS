368014a5e89adf0d39a0793af6afb0c545dd650b
Utkarsh Kushwaha <utkarsh.cyber01@gmail.com>
2026-08-25 10:34:12 +0530

    Add variable swap using temporary variable

---
+++ operators/swap_two_var_using_temp.js
@@ -0,0 +1,14 @@
+let a=5
+let b=10
+
+console.log("Before Swap :--")
+console.log("a = "+ a)
+console.log("b = "+ b)
+
+var temp=a
+a=b
+b=temp
+
+console.log("After Swap :--")
+console.log("a = "+ a)
+console.log("b = "+ b)
