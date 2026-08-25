f6615c5b849a2cb7605f3db6cf360b9482145fc7
Utkarsh Kushwaha <utkarsh.cyber01@gmail.com>
2026-08-25 10:34:12 +0530

    Add integer sum input program

---
+++ operators/input_int_sum.js
@@ -0,0 +1,8 @@
+let prompt=require('prompt-sync')()
+
+var a = Number(prompt("Enter The First Number : "))
+var b = Number(prompt("Enter The Second Number : "))
+
+var sum= a+b
+
+console.log(`Sum of ${a} and ${b} is ${sum}`)
