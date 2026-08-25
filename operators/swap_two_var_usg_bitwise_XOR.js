b8d2857a6272d8e0f5fc8ca618de86631253fd77
Utkarsh Kushwaha <utkarsh.cyber01@gmail.com>
2026-08-25 10:34:15 +0530

    Add variable swap using Bitwise XOR

---
+++ operators/swap_two_var_usg_bitwise_XOR.js
@@ -0,0 +1,20 @@
+let a = 5;
+let b = 10;
+
+
+
+console.log("Before Swap :--");
+console.log("a = " + a);
+console.log("b = " + b);
+
+
+a = a ^ b;
+b = a ^ b;
+a = a ^ b;
+
+
+console.log("After Swap :--");
+console.log("a = " + a);
+console.log("b = " + b);
+
+
