91f274a20c9415379e4cb37f05e7d53585608a4f
Utkarsh Kushwaha <utkarsh.cyber01@gmail.com>
2026-08-25 10:34:16 +0530

    Add middle index using Math.floor

---
+++ operators/middle_index.js
@@ -0,0 +1,11 @@
+
+console.log("When there is even index in array")
+let arr = [10, 20, 30, 40];
+let n = arr.length;
+console.log(`Middle index is ${Math.floor(n / 2)}`)
+
+
+console.log("When there is odd index in array")
+let arr2 = [10, 20, 30, 40, 50];
+let m = arr.length;
+console.log(`Middle index is ${Math.floor(m / 2)}`)
