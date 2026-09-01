cc657c3c9d84d5ceda45703323467c119db45e63
Utkarsh Kushwaha <utkarsh.cyber01@gmail.com>
2026-09-01 14:03:25 +0530

    calculate eletricity bills brute force approach

---
+++ Conditional_Statements/B_electricity_bill.js
@@ -0,0 +1,24 @@
+let prompt = require("prompt-sync")();
+
+let reading = Number(prompt("Enter Meter Reading :- "));
+let bill=0;
+
+if(reading >0 && reading<=100){
+    bill=100*4.2
+    console.log(bill);
+}
+else if(reading >100 && reading<=200){
+    bill=(100*4.2)+(reading-100)*6
+    console.log(bill);
+}
+
+else if(reading >200 && reading<=400){
+    bill=(100*4.2)+(100*6)+(reading-200)*8
+    console.log(bill);
+}
+
+else {
+    bill=(100*4.2)+(100*6)+(200*8)+(reading-400)*13
+    console.log(bill);
+}
+
