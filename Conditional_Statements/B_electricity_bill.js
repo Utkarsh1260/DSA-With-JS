81dc02238a9741bddf92f858261dec6f51f287e3
Utkarsh Kushwaha <utkarsh.cyber01@gmail.com>
2026-09-02 09:06:59 +0530

    refactor: add integer, negative, and zero value validation

--- Conditional_Statements/B_electricity_bill.js
+++ Conditional_Statements/B_electricity_bill.js
@@ -3,22 +3,30 @@ let prompt = require("prompt-sync")();
 let reading = Number(prompt("Enter Meter Reading :- "));
 let bill=0;
 
+
+if(reading<=0 || !Number.isInteger(reading) ) 
+    { console.log("Invalid Meter Reading !!!!") ;
+        console.log("Only Write Integer Reading, No decimal Numbers")
+        return ; }
+
+
 if(reading >0 && reading<=100){
-    bill=100*4.2
+    bill=reading*4.2;
     console.log(bill);
 }
 else if(reading >100 && reading<=200){
-    bill=(100*4.2)+(reading-100)*6
+    bill=(100*4.2)+(reading-100)*6;
     console.log(bill);
 }
 
 else if(reading >200 && reading<=400){
-    bill=(100*4.2)+(100*6)+(reading-200)*8
+    bill=(100*4.2)+(100*6)+(reading-200)*8;
     console.log(bill);
 }
 
 else {
-    bill=(100*4.2)+(100*6)+(200*8)+(reading-400)*13
+    bill=(100*4.2)+(100*6)+(200*8)+(reading-400)*13;
     console.log(bill);
 }
+
 
